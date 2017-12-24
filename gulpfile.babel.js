4411cea858f3d43bcfc0f0b753633c5df40393ef
VsevolodKurochka <dramdoobas123456@gmail.com>
2017-12-24 18:28:18 +0200

    Update GulpFile & Improve Speed of PUG & Add Image optimizer

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,42 +1,52 @@
 'use strict';
 
-// General
+
+/**
+ * Set paths
+ * -----------------------------------------------------------------------------
+ */
+
+const folders = {
+	build: 'build',
+	src: 'src'
+};
+
+
+/**
+ * General
+ * -----------------------------------------------------------------------------
+ */
 
 import gulp 						from 'gulp';
 import data 						from 'gulp-data';
 import install 					from 'gulp-install';
 import notify 					from 'gulp-notify';
 import fs 							from 'fs';
+import path 						from 'path';
 
-// BrowserSync
+
+/**
+ * browserSync
+ * -----------------------------------------------------------------------------
+ */
+
 import browserSync 			from 'browser-sync';
-const server = browserSync.create();
 
-function reload(done) {
-	server.reload();
-	done();
-}
-function serve(done) {
-	// Shows that run "watch" mode
-	global.watch = true;
-	
-	server.init({
-		server: {
-			baseDir: './app',
-			notify: true
-		}
-	});
-	done();
-}
 
-// PUG
+/**
+ * PUG
+ * -----------------------------------------------------------------------------
+ */
 
 import gulpif 					from 'gulp-if';
 import pug 							from 'gulp-pug';
-const emitty 			= require('emitty').setup('app', 'pug');
+const emitty 			= require('emitty').setup('src/views', 'pug');
 
 
-// Sass
+/**
+ * SASS
+ * -----------------------------------------------------------------------------
+ */
 
 import sass 						from 'gulp-sass';
 import sassGlob 				from 'gulp-sass-glob';
@@ -45,86 +55,171 @@ import csscomb 					from 'gulp-csscomb';
 import sassdoc 					from 'sassdoc';
 import converter				from 'sass-convert';
 
-// JS
+/**
+ * JS
+ * -----------------------------------------------------------------------------
+ */
 
 import babel 						from 'gulp-babel';
 import browserify 			from 'browserify';
 import source 					from 'vinyl-source-stream';
 import babelify 				from "babelify";
 
+/**
+ * Image
+ * -----------------------------------------------------------------------------
+ */
+
+import image from 'gulp-image';
 
 
-//- Install packages
+/**
+ * Init server
+ * -----------------------------------------------------------------------------
+ */
+
+const server = browserSync.create();
+
+function reload(done) {
+	server.reload();
+	done();
+}
+function serve(done) {
+	
+	global.watch = true;
+
+	server.init({
+		server: {
+			baseDir: `./${folders.build}`
+		},
+		notify: false
+	});
+
+	done();
+}
+
+
+/**
+ * Install packages
+ * -----------------------------------------------------------------------------
+ */
+
 gulp.src(['./package.json'])
 	.pipe(install())
 
 
-// SASS
-gulp.task('sass', () =>
-	gulp.src("app/sass/**/*.+(sass|scss)")
-		.pipe(sassGlob())
-		.pipe(sass({
-			outputStyle: 'expanded'
-		}).on('error', sass.logError))
-		.pipe(csscomb())
-		.pipe(autoprefixer({
-			browsers: ['last 15 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest("app/css"))
+/**
+ * SASS
+ * -----------------------------------------------------------------------------
+ */
+
+gulp.task('sass', () => gulp
+	.src(`${folders.src}/sass/**/*.+(sass|scss)`)
+	.pipe(sassGlob())
+	.pipe(sass({
+		outputStyle: 'expanded'
+	}).on('error', sass.logError))
+	.pipe(csscomb())
+	.pipe(autoprefixer({
+		browsers: ['last 15 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest(`${folders.build}/css`))
 );
+
+
+/**
+ * SASS (watch)
+ * -----------------------------------------------------------------------------
+ */
 gulp.task('sass:watch', () => {
-	gulp.watch("app/sass/**/*.+(sass|scss)", gulp.series('sass', reload));
+	gulp.watch(`${folders.src}/sass/**/*.+(sass|scss)`, gulp.series('sass', reload));
 });
 
 
-// PUG
+/**
+ * PUG
+ * -----------------------------------------------------------------------------
+ */
+
+// gulp.task('templates', () =>
+// 	new Promise((resolve, reject) => {
+// 		emitty.scan(global.emittyChangedFile).then(() => {
+// 			gulp.src(`${folders.src}/views/site/**/*.pug`)
+// 				.pipe(gulpif(global.watch, emitty.filter(global.emittyChangedFile)))
+// 				.pipe(pug({ basedir: `${__dirname}/${folders.src}/views`, pretty: true }))
+// 				.on('error', notify.onError({
+// 						title: 'PUG Error',
+// 						message: '<%= error.message %>'
+// 				}))
+// 				.pipe(gulp.dest(`${folders.build}`))
+// 				.on('end', resolve)
+// 				.on('error', reject);
+// 		});
+// 	})
+// );
+
+// gulp.task('templates:watch', () => {
+// 	gulp.watch(`${folders.src}/views/**/*.pug`, gulp.series('templates'))
+// 		.on('all', (event, filepath) => {
+// 			global.emittyChangedFile = filepath;
+// 		})
+// });
 gulp.task('templates', () =>
-	gulp.src('app/**/*.pug')
-		.pipe(gulpif(global.watch, emitty.stream(global.emittyChangedFile)))
-		.pipe(
-			pug({
-				pretty: true
-			})
-		)
+	gulp.src(`${folders.src}/views/site/**/*.pug`)
+		.pipe(pug({ basedir: `${__dirname}/${folders.src}/views`, pretty: true }))
 		.on('error', notify.onError({
 				title: 'PUG Error',
 				message: '<%= error.message %>'
 		}))
-		.pipe(gulp.dest('app'))
+		.pipe(gulp.dest(`${folders.build}`))
 );
 
 gulp.task('templates:watch', () => {
-	gulp.watch('app/**/*.pug', gulp.series('templates', reload))
+	gulp.watch(`${folders.src}/views/**/*.pug`, gulp.series('templates', reload))
 		.on('all', (event, filepath) => {
 			global.emittyChangedFile = filepath;
-		});
+		})
 });
+
+
+/**
+ * Optimize PNG, JPEG, GIF, SVG images.
+ * -----------------------------------------------------------------------------
+ */
+
+gulp.task('image', () =>
+	gulp.src(`${folders.src}/img/**/*`)
+		.pipe(image({
+			zopflipng: false
+		}))
+		.pipe(gulp.dest(`${folders.build}/img`))
+);
 
 // JS
 
-gulp.task('scripts', () =>
-	browserify({
-		entries: './app/babel/app.js',
-		debug: true
-	})
-	.transform(babelify)
-	.on('error', notify.onError({
-			title: 'babelify Error',
-			message: '<%= error.message %>'
-	}))
-	.bundle()
-	.on('error', notify.onError({
-			title: 'Bundle Error',
-			message: '<%= error.message %>'
-	}))
-	.pipe(source('bundle.js'))
-	.pipe(gulp.dest('app/js'))
-);
+// gulp.task('scripts', () =>
+// 	browserify({
+// 		entries: 'app/babel/app.js',
+// 		debug: true
+// 	})
+// 	.transform(babelify)
+// 	.on('error', notify.onError({
+// 			title: 'babelify Error',
+// 			message: '<%= error.message %>'
+// 	}))
+// 	.bundle()
+// 	.on('error', notify.onError({
+// 			title: 'Bundle Error',
+// 			message: '<%= error.message %>'
+// 	}))
+// 	.pipe(source('bundle.js'))
+// 	.pipe(gulp.dest('app/js'))
+// );
 
-gulp.task('scripts:watch', () => {
-	gulp.watch("app/babel/**/*.js", gulp.series('scripts', reload));
-});
+// gulp.task('scripts:watch', () => {
+// 	gulp.watch("app/babel/**/*.js", gulp.series('scripts', reload));
+// });
 
 
 // Your "watch" task
@@ -133,8 +228,16 @@ gulp.task(
 	gulp.parallel(
 		serve,
 		'sass:watch',
-		'scripts:watch',
-		'templates:watch'
+		'templates:watch',
+		'image'
+	)
+);
+
+gulp.task(
+	'build',
+	gulp.parallel(
+		'sass',
+		'templates'
 	)
 );
 
