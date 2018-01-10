f4d3e61d2bd106eafc295ab8036d440c48bc2d34
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-01-10 14:43:08 +0200

    Add Modal Class

--- src/js/vanilla.main.js
+++ src/js/vanilla.main.js
@@ -1,102 +1,145 @@
 // General functions
-	function log(content){
-		console.log(content);
+function log(content){
+	console.log(content);
+}
+
+var hasClass = (element, cls) => (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+
+function addClass(element,cls){
+	if( !hasClass(element, cls) ){
+		let empty = '';
+		if(element.classList.value != "") empty = ' ';
+		element.className += empty + cls;
+	}
+}
+
+function removeClass(element, cls){
+	if( hasClass(element, cls) ) element.classList.remove(cls);
+}
+
+function toggleClass(element, cls){
+	hasClass(element, cls) ? removeClass(element, cls) : addClass(element, cls);
+}
+
+var exists = element => typeof(element) != 'undefined' && element != null;
+
+class Modal {
+	constructor(){
+
+		// Prefix for modal class
+		this.prefix = '';
+
+		// Name of modal class
+		this.name = `${this.prefix}modal`;
+
+		// All modals
+		this.modals = document.querySelectorAll(`.${this.name}`);
+
+		// Open Buttons
+		this.buttons = document.querySelectorAll(`[data-action="${this.name}"]`);
+
+		// Close Button(`x`)
+		this.closeButtons = document.querySelectorAll(`[data-close="${this.name}"]`);
+
+		this.buttons.forEach( (button) => {
+			button.addEventListener('click', (e) => this._showButtonClick(e, this));
+		});
+
+		this.closeButtons.forEach( (button) => {
+			button.addEventListener('click', (e) => this._closeButtonClick(e, this));
+		});
+
+		this.bodyEvents = ['click', 'touchstart'];
+
+		this.bodyEvents.forEach( (event) => {
+			document.body.addEventListener(event, (e) => {
+				this._bodyClick(e, this);
+			});
+		});
 	}
 
-	var hasClass = (element, cls) => (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 
-	function addClass(element,cls){
-		if( !hasClass(element, cls) ){
-			let empty = '';
-			if(element.classList.value != "") empty = ' ';
-			element.className += empty + cls;
+	modalClose(el){
+		removeClass(el, `${this.name}_showing_in`);
+		removeClass(document.body, `${this.name}-open`);
+	}
+
+	modalOpen(el){
+		addClass(document.body, `${this.name}-open`);
+		addClass(el, `${this.name}_showing_in`);
+	}
+
+	_showButtonClick(e) {
+		// Get button data-attributes
+		var modalData = e.target.dataset;
+
+		// Get attribute data-open and replace # with empty line
+		var modalID = modalData.open.replace("#", "");
+		
+		
+		if( exists(document.getElementById(modalID) ) ){
+
+			let modalCurrent = document.getElementById(modalID);
+
+			this.modalOpen(modalCurrent);
+
+			// if(modalData.video != undefined){
+			// 	let videoSRC = modalData.video;
+			// 	let videoWrapper = modalCurrent.getElementsByClassName('v-modal__video')[0];
+
+			// 	videoWrapper.innerHTML = '';
+
+			// 	let videoIframe = document.createElement('iframe');
+
+			// 	addClass(videoIframe, 'v-modal__iframe');
+			// 	videoIframe.setAttribute('src', videoSRC);
+			// 	videoWrapper.appendChild(videoIframe);
+			// }
+
+		}else{
+			console.error('No element with ID: ' + modalID);
 		}
 	}
 
-	function removeClass(element, cls){
-		if( hasClass(element, cls) ) element.classList.remove(cls);
+	_closeButtonClick(e) {
+		this.modalClose( e.target.closest(`.${this.name}`) );
 	}
 
-	function toggleClass(element, cls){
-		hasClass(element, cls) ? removeClass(element, cls) : addClass(element, cls);
+	_getEventTarget(e){
+		var targ;
+
+		if (e.target) { // W3C
+			targ = e.target;
+		}else if (e.srcElement) { // IE6-8
+			targ = e.srcElement;
+		}else if(e.originalTarget){
+			targ = e.originalTarget;
+		}
+		if (targ.nodeType == 3) { // Safari
+			targ = targ.parentNode;
+		}
+		return targ;
 	}
 
-	var exists = element => typeof(element) != 'undefined' && element != null;
+	_bodyClick(e){
+		let target = this._getEventTarget(e);
+
+		//log(target);
+		this.modals.forEach( (modal) => {
+			if(target == modal){
+				this.modalClose(modal);
+			}
+		});
+	}
+}
+
 
 (function(){
 	document.addEventListener("DOMContentLoaded", function(){
 
 		const themePrefix = '';
 
-		class Modal {
-			constructor(){
-
-				// Prefix for modal class
-				this.prefix = '';
-
-				// Name of modal class
-				this.name = `${this.prefix}modal`;
-
-				// All modals
-				this.modals = document.querySelectorAll(`.${this.name}`);
-
-				// Open Buttons
-				this.buttons = document.querySelectorAll(`[data-action="${this.name}"]`);
-
-				// Close Button(`x`)
-				this.closeButtons = document.querySelectorAll(`[data-close="${this.name}"]`);
-
-				this.buttons.forEach( (button) => {
-					button.addEventListener('click', (e) => this.showButtonClick(e, this));
-				});
-
-				this.closeButtons.forEach( (button) => {
-					button.addEventListener('click', (e) => this.closeButtonClick(e, this));
-				});
-			}
-
-			showButtonClick(e) {
-				// Get button data-attributes
-				var modalData = e.target.dataset;
-
-				// Get attribute data-open and replace # with empty line
-				var modalID = modalData.open.replace("#", "");
-				
-				
-				if( exists(document.getElementById(modalID) ) ){
-
-					let modalCurrent = document.getElementById(modalID);
-
-					addClass(document.body, `${this.name}-open`);
-					addClass(modalCurrent, `${this.name}_showing_in`);
-
-					// if(modalData.video != undefined){
-					// 	let videoSRC = modalData.video;
-					// 	let videoWrapper = modalCurrent.getElementsByClassName('v-modal__video')[0];
-
-					// 	videoWrapper.innerHTML = '';
-
-					// 	let videoIframe = document.createElement('iframe');
-
-					// 	addClass(videoIframe, 'v-modal__iframe');
-					// 	videoIframe.setAttribute('src', videoSRC);
-					// 	videoWrapper.appendChild(videoIframe);
-					// }
-
-				}else{
-					console.error('No element with ID: ' + modalID);
-				}
-			}
-
-			modalClose(el){
-				removeClass(el, `${this.name}_showing_in`);
-				removeClass(document.body, `${this.name}-open`);
-			}
-
-			closeButtonClick(e) {
-				this.modalClose( e.target.closest(`.${this.name}`) );
-			}
-		}
+		
 
 		new Modal();
 		//console.log(new Modal());
@@ -122,21 +165,7 @@
 		// 	// 	el.getElementsByClassName('vmodal__video')[0].innerHTML = '';
 		// 	// }
 		// }
-		// function getEventTarget(e){
-		// 	var targ;
-	
-		// 	if (e.target) { // W3C
-		// 		targ = e.target;
-		// 	}else if (e.srcElement) { // IE6-8
-		// 		targ = e.srcElement;
-		// 	}else if(e.originalTarget){
-		// 		targ = e.originalTarget;
-		// 	}
-		// 	if (targ.nodeType == 3) { // Safari
-		// 		targ = targ.parentNode;
-		// 	}
-		// 	return targ;
-		// }
+
 		// function bodyClick(e){
 		// 	let target = getEventTarget(e);
 		// 	for(let i = 0; i < modalL; i++){
